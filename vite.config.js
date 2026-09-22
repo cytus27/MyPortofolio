29b8510eec4b6635f8d606be21f2fdbe17760df6
Aldo <aldo30062004@gmail.com>
2026-09-22 12:31:36 +0700

    Mengupdate css

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
+import path from 'path';
+import os from 'os';
 
 export default defineConfig({
   base: '/MyPortofolio/',
   build: {
     outDir: 'dist',
   },
+  cacheDir: path.join(os.tmpdir(), 'vite-cache-myportofolio'),
 });
